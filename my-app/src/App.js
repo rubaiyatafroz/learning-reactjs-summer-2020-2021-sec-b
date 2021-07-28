8f5f4b3b0da607ecb52bcc075aa3f7e4d45d1634
Rubaiyat <ummulmery320@gmail.com>
2021-07-28 23:35:39 +0600

    add update

---
+++ my-app/src/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import UserList from "./components/UserList";
+import Navbar from "./components/NavBar";
+import AddUser from "./components/AddUser";
+import { users } from "./usersData";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+    const [myuser, setUsers] = useState(users);
+
+    const addUsers = (newUser) => {
+        setUsers([...myuser, newUser]);
+    };
+
+    const editUsers = (newUser) => {
+        const data = myuser.filter((user) => user.id != newUser.id);
+        setUsers([...data, newUser]);
+    };
+    const deleteCallback = (id) => {
+        const data = myuser.filter((user) => user.id !== id);
+        setUsers(data);
+    };
+
+    // const url = "http://localhost:8000/userlist";
+    // const response = fetch(url);
+    // console.log(response);
+
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route exact path="/">
+                    <h2>Welcome Home</h2>
+                </Route>
+                <Route path="/userlist">
+                    <div>
+                        <UserList list={myuser} callback={deleteCallback} />
+                    </div>
+                </Route>
+                <Route path="/Create">
+                    <AddUser status="add" callback={addUsers} />
+                </Route>
+                <Route path="/edit/:id">
+                    <AddUser status="edit" callback={editUsers} />
+                </Route>
+                <Route path="*">
+                    <h3>404 not found</h3>
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
---
+++ my-app/src/usersData.js
@@ -0,0 +1,22 @@
+export const users =[
+    {
+        id:1 ,
+        name: "Sumu",
+        dept: "se"
+    },
+    {
+        id:2 , 
+        name: "Mumu", 
+        dept: "cse"
+    },
+    {
+        id:3 , 
+        name: "rayhan", 
+        dept: "cse"
+    },
+    {
+        id:4 ,
+        name: "shanto", 
+        dept: "cis"
+    }
+];
